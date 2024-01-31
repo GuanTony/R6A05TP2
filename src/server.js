57de8b28d60de47f3989d1387715c0d3e82c1e00
Tony <tony.guan@etu.u-paris.fr>
2024-01-31 12:27:53 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,4 +1,11 @@
 import {getData} from "./api.js";
+import {fastifyview} from "@fastify/view";
 
 console.log("Je suis Marvel")
 console.log(await getData("https://gateway.marvel.com/v1/public/characters"))
+
+fastify.register(require("@fastify/view")),{
+    engine : {
+        handlebars : require("handlebars"),
+    }
+};
