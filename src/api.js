d5029021429898ef3ff1982b5e3de337509b792c
Tony <tony.guan@etu.u-paris.fr>
2024-01-31 11:59:35 +0100

    Etape 3

--- src/api.js
+++ src/api.js
@@ -1,4 +1,8 @@
+import {createHash} from 'node:crypto'
 
+const apiKey = "7622f790bf1f73538ed1a4ec3cf673b9";
+const privateKey = "c4b567f595e83993c6652fe38e4e5ba3f061be66";
+const ts = new Date().getTime();
 
 /**
  * Récupère les données de l'endpoint en utilisant les identifiants
@@ -7,13 +11,28 @@
  * @return {Promise<json>}
  */
 export const getData = async (url) => {
-    let response = await fetch("https://gateway.marvel.com:443/v1/public/characters?apikey={{publicKey}}&ts={{ts}}&hash={{hash}}")
-    if (response.ok) {
-        return await response.json()
-    } else {
-        throw new Error("HTTP-Error: " + response.status)
-    }
+    const response = await fetch(url + "?" + new URLSearchParams({
+        apikey: apiKey,
+        hash: await getHash(apiKey, privateKey, ts),
+        ts: ts,
+        limit: 100
+    }));
 
+    const responseJson = await response.json();
+    const responseResults = responseJson.data.results;
+    let responseWithThumbnail = []
+
+    responseResults.forEach((element) => {
+        if(element.thumbnail.path !== 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available') {
+            responseWithThumbnail.push(element);
+        }
+    });
+
+    return responseWithThumbnail.map((character) => {
+        const newCharacter = { ...character };
+        newCharacter.imageUrl = character.thumbnail.path + "/portrait_xlarge." + character.thumbnail.extension
+        return newCharacter;
+    });
 }
 
 /**
@@ -25,8 +44,5 @@ export const getData = async (url) => {
  * @return {Promise<ArrayBuffer>} en hexadecimal
  */
 export const getHash = async (publicKey, privateKey, timestamp) => {
-    publicKey = "7622f790bf1f73538ed1a4ec3cf673b9"
-    privateKey = "c4b567f595e83993c6652fe38e4e5ba3f061be66"
-    timestamp = Date.now()
-
+    return createHash('md5').update(timestamp + privateKey + publicKey).digest("hex")
 }
--- src/server.js
+++ src/server.js
@@ -1,1 +1,4 @@
+import {getData} from "./api.js";
+
 console.log("Je suis Marvel")
+console.log(await getData("https://gateway.marvel.com/v1/public/characters"))
