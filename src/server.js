68f5a634dfba1dfe9bff80c169f4628ea0884fdc
Tony <tony.guan@etu.u-paris.fr>
2024-02-02 18:44:41 +0100

    etape 4 sans header et footer

--- src/server.js
+++ src/server.js
@@ -1,11 +1,38 @@
 import {getData} from "./api.js";
-import {fastifyview} from "@fastify/view";
+import Fastify from 'fastify'
+import handlebars from 'handlebars';
+import fastifyView from '@fastify/view';
 
 console.log("Je suis Marvel")
 console.log(await getData("https://gateway.marvel.com/v1/public/characters"))
 
-fastify.register(require("@fastify/view")),{
-    engine : {
-        handlebars : require("handlebars"),
+
+const fastify = Fastify({
+    logger: true
+})
+
+fastify.register(fastifyView, {
+    engine: {
+        handlebars,
+    },
+    includeViewExtension: true,
+    templates: 'templates',
+    options: {
+        partials: {
+            header: 'header.hbs',
+            footer: 'footer.hbs'
+        }
     }
-};
+})
+
+fastify.get('/', async function handler (request, reply) {
+    const marvelChar = await getData("https://gateway.marvel.com:443/v1/public/characters")
+    return reply.view('index.hbs', { marvelChar })
+})
+
+try {
+    await fastify.listen({ port: 3000 })
+} catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+}
